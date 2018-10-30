5f1e922251b6cdd576d5fd94f27fd2b15fdc33c7
April Copes <antdevelopment1@gmail.com>
2018-10-30 15:16:19 -0400

    Adds event listener for return key, checkmark button and stores functionality in a reusable function

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,48 @@
-// Stores an annonymous function inside the budgetController variable
+// Budget Controller
 var budgetController = ( function() {
 
-    // Private Variables trapped inside our annonymous function that has been Immediatly Invoked
-    var x = 23;
-
-    // Private function trapped inside our annonymous function that has been immediatly invoked
-    var add = function(a) {
-        return x + a;
-    }
-
-    // Public object returned from our IIFE that gives us access to our publicTest method
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
+   
+    
 }());
 
+// UI Controller
 var UIController = (function() {
-
+ 
     // Some code
 
 }());
 
-
+// Global App Controller
 // Main function that will tied to both of our modular functions (UIController, budgetController)
 var controller = (function(budgetCtrl, UICtrl) {
 
-    // Private variable trapped inside our IIFE
-    var z = budgetCtrl.publicTest(5)
+    var ctrlAddItem = function() {
 
-    // Public object returned from our closure giving us access to our anotherPublic method
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
+        // 1. Get the field input data
+
+        // 2. Add the item to the budget controller
+
+        // 3. Add the new item to the User Interface
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the User Interface
+
+        console.log('It works');
+
     }
 
-    // IIFE that accepts two other IIFE. This ties both IIFE's together giving us access to both private and public variables and methods
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    // The event parameter exists for all event listerners. We did not create it.
+    // event.which is to test for older browsers which do not have the keyCode property
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+
+            ctrlAddItem();
+            
+        }
+    })
+  
 }(budgetController, UIController));
