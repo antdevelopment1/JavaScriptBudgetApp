45b5b9263527193471db3d9823577ccdc9da6d60
April Copes <antdevelopment1@gmail.com>
2018-10-30 16:36:56 -0400

    Returned query selectors in a public object saving the classNames for querySelectors in a private object, also adds functionality for expense type, description and value(cost)

--- scripts/index.js
+++ scripts/index.js
@@ -7,8 +7,30 @@ var budgetController = ( function() {
 
 // UI Controller
 var UIController = (function() {
+
+    var DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
  
-    // Some code
+    return {
+        getinput: function() {
+
+            return {
+                
+                type: document.querySelector(DOMStrings.inputType).value, // Will be either income or expense
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
+
+            }
+        },
+
+        getDOMStrings: function() {
+            return DOMStrings;
+        }
+    };
 
 }());
 
@@ -16,10 +38,13 @@ var UIController = (function() {
 // Main function that will tied to both of our modular functions (UIController, budgetController)
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMStrings();
+
     var ctrlAddItem = function() {
 
         // 1. Get the field input data
-
+        var input = UICtrl.getinput();
+        console.log(input);
         // 2. Add the item to the budget controller
 
         // 3. Add the new item to the User Interface
@@ -28,12 +53,10 @@ var controller = (function(budgetCtrl, UICtrl) {
 
         // 5. Display the budget on the User Interface
 
-        console.log('It works');
-
     }
 
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     // The event parameter exists for all event listerners. We did not create it.
     // event.which is to test for older browsers which do not have the keyCode property
